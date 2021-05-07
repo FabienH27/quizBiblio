fdc9a1a8759e974b4e5290af4da027756ceabf45
Fabien <fhannon60@gmail.com>
2021-05-07 15:28:05 +0200

    register form added

--- quizBiblio/quizApps/views.py
+++ quizBiblio/quizApps/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
+from .forms import RegisterForm
 
 def index(request):
     return render(request, 'quizApps/index.html')
@@ -7,4 +11,16 @@ def login(request):
     return render(request, 'quizApps/login.html')
 
 def register(request):
-    return render(request, 'quizApps/register.html')
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username, password=raw_password)
+            login(user)
+            return redirect('index')
+    else:
+        form = RegisterForm()
+
+    return render(request, 'quizApps/register.html', {'form': form})
