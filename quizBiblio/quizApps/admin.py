ff84b57ec3c0e2c3db81ab1f2ee030cca9b3a259
Fabien <fhannon60@gmail.com>
2021-05-10 14:39:13 +0200

    scafolding database architecture

--- quizBiblio/quizApps/admin.py
+++ quizBiblio/quizApps/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import CustomUser
+from .models import CustomUser, Quiz, UserQuiz, Question, Proposition
 
 # Register your models here.
 
 admin.site.register(CustomUser)
+admin.site.register(Quiz)
+admin.site.register(UserQuiz)
+admin.site.register(Question)
+admin.site.register(Proposition)
--- quizBiblio/quizApps/models.py
+++ quizBiblio/quizApps/models.py
@@ -4,3 +4,27 @@ from django.utils.translation import gettext_lazy as _
 
 class CustomUser(AbstractUser):
     email = models.EmailField(_('email address'), help_text='Requis. Saisir une adresse email valide.', unique=True)
+
+class Quiz(models.Model):
+    title = models.CharField(_('title'), max_length=30)
+    theme = models.CharField(_('theme'), max_length=255)
+    image = models.ImageField(_('image'), blank=True, null=True)
+    user = models.ManyToManyField(CustomUser, through='UserQuiz', related_name='quiz')
+
+class UserQuiz(models.Model):
+    score = models.IntegerField(_('score'), blank=True, null=True)
+    time = models.DateTimeField(_('time'), auto_now_add=False,blank=True, null=True)
+    is_creator = models.BooleanField(default=False)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+
+class Question(models.Model):
+    questionText = models.CharField(_('question'), max_length=255)
+    image = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    description = models.TextField(_('description'))
+    quiz = models.ManyToManyField(Quiz)
+
+class Proposition(models.Model):
+    propositionText = models.CharField(_('proposition'), max_length=255)
+    image = models.ImageField(upload_to='uploads/',blank=True, null=True)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
