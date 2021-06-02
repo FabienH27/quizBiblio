725278c5dbed6afde8612f5fa2496bab98b60781
Fabien <fhannon60@gmail.com>
2021-06-02 20:48:55 +0200

    rollback to previous architecture

--- quizBiblio/quizApps/apps.py
+++ quizBiblio/quizApps/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 
 
-class QuizAppsConfig(AppConfig):
+class QuizappsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'quizApps'
