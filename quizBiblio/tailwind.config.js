b43d31191678acef6fe932c9f65790a44109d2cb
Fabien H <fhannon60@gmail.com>
2022-01-26 19:41:11 +0100

    Optimize css imports

--- quizBiblio/tailwind.config.js
+++ quizBiblio/tailwind.config.js
@@ -1,37 +1,54 @@
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 
 module.exports = {
-  purge: {
-    enabled:false,
-    preserveHtmlElements: false,
-    content: ['./quizApps/templates/quizApps/*.html']
-  },
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      white: colors.white,
-      teal: colors.teal,
-      gray: colors.coolGray,
-      red: colors.red,
+    purge: {
+        enabled: true,
+        preserveHtmlElements: false,
+        content: ["./quizApps/templates/quizApps/*.html"],
     },
-    fontFamily: {
-      title: [
-        'Fira Sans', 'Open Sans', 'Nunito Sans','sans-serif','Arial','Segoe UI','ui-sans-serif','system-ui', '-apple-system', 'BlinkMacSystemFont'
-      ],
-      display: [
-        'Karla', 'Nunito',' Halant', 'PT Serif','sans-serif','Arial','system-ui','-apple-system', 'BlinkMacSystemFont'
-      ]
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        colors: {
+            transparent: "transparent",
+            current: "currentColor",
+            white: colors.white,
+            teal: colors.teal,
+            gray: colors.gray,
+            red: colors.red,
+        },
+        fontFamily: {
+            title: [
+                "Fira Sans",
+                "Open Sans",
+                "Nunito Sans",
+                "sans-serif",
+                "Arial",
+                "Segoe UI",
+                "ui-sans-serif",
+                "system-ui",
+                "-apple-system",
+                "BlinkMacSystemFont",
+            ],
+            display: [
+                "Karla",
+                "Nunito",
+                " Halant",
+                "PT Serif",
+                "sans-serif",
+                "Arial",
+                "system-ui",
+                "-apple-system",
+                "BlinkMacSystemFont",
+            ],
+        },
+        extend: {
+            screens: {
+                "3xl": "2500px",
+            },
+        },
     },
-    extend:{
-      screens:{
-        '3xl':'2500px'
-      }
-    }
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
